ab95964fa442ab0ee6d6563480365892d6a9efb8
Martasya Dwi Pramesty <martasyacaa11@gmail.com>
2022-10-31 20:22:06 +0700

    inicial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+  echo "Hello World!";
+
+  $nama = "Martasya";
+  $umur = 20;
+
+  echo "Nama saya <strong>$nama</strong>, saya berusia $umur tanun.";
+
+  $namaAdik = "Aca";
+  $umurAdik = "12";
+ 
+  echo "Nama Adik saya <strong>$nama</strong>, Adik saya berusia $umur tahun.<br>"
+
+  $selisihUmur = $umur - $umurAdik;
+  echo "Selisih usia saya dan Adik saya $selisihUmur tahun."
+?>
